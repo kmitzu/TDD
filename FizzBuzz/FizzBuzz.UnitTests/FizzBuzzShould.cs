fc5a410b4b40b9b1efdccb18b941e594eaccd578
kmitzu <camelia.hofnar@yahoo.com>
2017-02-08 12:44:48 +0000

    added intial files

---
+++ FizzBuzz/FizzBuzz.UnitTests/FizzBuzzShould.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace FizzBuzz.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzShould
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(99, "Fizz")]
+        public void ReturnTheCorrectFizzBuzzConversion(int number, string expectedString)
+        {
+            var fizzBuzz = new FizzBuzz();
+
+            var fizzBuzzOutput = fizzBuzz.Convert(number);
+
+            Assert.That(fizzBuzzOutput, Is.EqualTo(expectedString));
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,25 @@
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Convert(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return number.ToString();
+        }
+    }
+}
