4c95cf91bce4e74b96e75e9bf7fc234186caa1e2
kmitzu <camelia.hofnar@yahoo.com>
2017-02-09 16:04:54 +0000

    Added String Calculator

---
+++ StringCalculator/StringCalculator.Tests/StringCalculatorShould.cs
@@ -0,0 +1,76 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using NUnit.Framework;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorShould
+    {
+        [Test]
+        public void ReturnZeroWhenStringIsEmpty()
+        {
+            var calculator = new Calculator();
+
+            int expectedSum = calculator.CalculateFromString("");
+
+            Assert.AreEqual(expectedSum, 0);
+        }
+
+        [Test]
+
+        public void ReturnOneWhenStringIsOne()
+        {
+            var calculator = new Calculator();
+
+            int expectedSum = calculator.CalculateFromString("1");
+
+            Assert.AreEqual(expectedSum, 1);
+        }
+
+        [Test]
+
+        public void ReturnTwoWhenStringIsTwo()
+        {
+            var calculator = new Calculator();
+
+            int expectedSum = calculator.CalculateFromString("2");
+
+            Assert.AreEqual(expectedSum, 2);
+        }
+
+        [Test]
+        public void ReturnThreeWhenStringIsThree()
+        {
+            var calculator = new Calculator();
+
+            int expectedSum = calculator.CalculateFromString("3");
+
+            Assert.AreEqual(expectedSum, 3);
+        }
+
+        [TestCase("", 0)]
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        public void ReturnIntegerValueOfString(string input, int result)
+        {
+            var calculator = new Calculator();
+
+            int expectedSum = calculator.CalculateFromString(input);
+
+            Assert.AreEqual(expectedSum, result);
+        }
+    }
+
+    public class Calculator     
+    {
+        public int CalculateFromString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+
+            return int.Parse(input);
+        }
+    }
+}
---
+++ StringCalculator/UnitTests/PrintNumberShould.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SimpleApp;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class PrintNumberShould
+    {
+        private FormatNumber _formatNumber;
+
+        [SetUp]
+        public void Setup()
+        {
+            _formatNumber = new FormatNumber();
+        }
+
+
+        [TestCase(9)]
+        [TestCase(6)]
+        [TestCase(3)]
+        public void ReturnHelloWhenTheNumberIsDivisibleByThree(int number)
+        {
+            //Assert
+            string numberToString = _formatNumber.Print(number);
+
+            //Act
+            Assert.AreEqual("Hello", numberToString);
+        }
+
+        [TestCase(5)]
+        [TestCase(10)]
+        public void ReturnHelloWhenTheNumberIsDivisibleByFive(int number)
+        {
+            //Assert
+            string numberToString = _formatNumber.Print(number);
+
+            //Act
+            Assert.AreEqual("Goodbye", numberToString);
+        }
+
+        [TestCase(15)]
+        public void ReturnHelloGoodByeWhenTheNumberIsDivisibleByFiveAndThree(int number)
+        {
+            //Assert
+            string numberToString = _formatNumber.Print(number);
+
+            //Act
+            Assert.AreEqual("HelloGoodbye", numberToString);
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(4)]
+        [TestCase(13)]
+        public void ReturnActualNumberWhenTheNumberIsNotDivisibleByFiveAndIsNotDivisibleByThree(int number)
+        {
+            //Assert
+            string numberToString = _formatNumber.Print(number);
+
+            //Act
+            Assert.AreEqual(number.ToString(), numberToString);
+        }
+    }
+}
