de70239d4595cbee2ad3b3f63f11d3ea5f355f4f
kmitzu <camelia.hofnar@yahoo.com>
2017-02-09 15:58:15 +0000

    Added Leap Year

---
+++ LeapYear/LeapYear.Tests/LeapYearShould.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace LeapYear.Tests
+{
+    [TestFixture]
+    public class LeapYearShould
+    {
+        public LeapYearValidator LeapYearValidator { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            this.LeapYearValidator = new LeapYearValidator();
+        }
+
+        [TestCase(2000)]
+        [TestCase(1996)]
+        [TestCase(2004)]
+        [TestCase(6000)]
+        [TestCase(4000)]
+       
+       
+        public void ReturnTrueForALeapYear(int year)
+        {
+            int testValue = year;
+            
+            var result = LeapYearValidator.LeapYear(testValue);
+
+            Assert.IsTrue(result);
+        }
+
+         [TestCase(2001)]
+         [TestCase(1000)]
+         [TestCase(100)]
+        public void ReturnFalseForACommonYear(int year)
+        {
+            var result = LeapYearValidator.LeapYear(year);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void ReturnFalseForAnAtypicalYear()
+        {
+            int testValue = 1900;
+
+            var result = LeapYearValidator.LeapYear(testValue);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ LeapYear/LeapYear/LeapYearValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    public class LeapYearValidator
+    {
+        public bool LeapYear(int value)
+        {
+            if (value % 4 == 0)
+            {
+                if (value % 100 == 0)
+                {
+                    if (value % 400 == 0)
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                return true;
+            }
+
+            return false;
+
+            //if (value % 4 == 0 && value%100 == 0 && value%400 == 0)
+            //{
+            //    return true;
+            //}
+            //if (value%4 == 0 && value%100 !=0)
+            //{
+            //    return true;
+            //}
+            //return false;
+        }
+    }
+}
